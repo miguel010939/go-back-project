41eae216bfadd2bf917c0cb4c23cb990156c9387
miguel <miguel@example.com>
2024-05-21 19:36:40 +0200

    models for JSON serializing & deserializing

--- models/product.go
+++ models/product.go
@@ -1,1 +1,15 @@
 package models
+
+type ProductRepresentation struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ImageUrl    string `json:"imageUrl"`
+	UserID      int    `json:"userId"`
+}
+
+type ProductForm struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	ImageUrl    string `json:"imageUrl"`
+}
--- models/user.go
+++ models/user.go
@@ -4,3 +4,19 @@ type User struct {
 	ID       int    `json:"id"`
 	Username string `json:"username"`
 }
+
+type UserRepresentation struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
+
+type UserSignUpForm struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type UserLogInForm struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
