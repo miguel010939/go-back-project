94563168362621c19d2fd97a57a1ee64daf9a942
miguel <miguel@example.com>
2024-05-24 21:47:10 +0200

    bid repo

--- repositories/bidrepo.go
+++ repositories/bidrepo.go
@@ -1,6 +1,9 @@
 package repositories
 
-import "database/sql"
+import (
+	"database/sql"
+	"errors"
+)
 
 type BidRepo struct {
 	db *sql.DB
@@ -11,10 +14,48 @@ func NewBidRepo(db *sql.DB) *BidRepo {
 }
 
 func (r *BidRepo) MakeBid(userId int, productId int, amount float32) error {
-
+	if amount <= 0 {
+		return InvalidInput
+	}
+	getProductQuery := "SELECT id FROM products WHERE id = $1"
+	err := r.db.QueryRow(getProductQuery, productId).Scan()
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return NotFound
+		}
+		return SomethingWentWrong
+	}
+	// i dont need to check userId because it comes from the token
+	insertQuery := `INSERT INTO bids (userx, product, amount) 
+					VALUES ($1, $2, $3)`
+	_, err2 := r.db.Exec(insertQuery, userId, productId, amount)
+	if err2 != nil {
+		return SomethingWentWrong
+	}
 	return nil
 }
 func (r *BidRepo) DeleteBid(userId int, bidId int) error {
-
+	getBidQuery := "SELECT id FROM bids WHERE id = $1"
+	err := r.db.QueryRow(getBidQuery, bidId).Scan()
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return NotFound
+		}
+		return SomethingWentWrong
+	}
+	// again, i dont need to check userId because it comes from the token
+	deleteQuery := `DELETE FROM bids 
+					WHERE userx=$1 AND id=$2`
+	result, err1 := r.db.Exec(deleteQuery, userId, bidId)
+	if err1 != nil {
+		return SomethingWentWrong
+	}
+	rows, err2 := result.RowsAffected()
+	if err2 != nil {
+		return SomethingWentWrong
+	}
+	if rows == 0 {
+		return NotFound
+	}
 	return nil
 }
