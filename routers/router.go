7840868ea54f0c3fe1874fa40057f9c199b5a74d
miguel <miguel@example.com>
2024-05-26 19:25:50 +0200

    oops, forgot the methods

--- routers/router.go
+++ routers/router.go
@@ -9,29 +9,30 @@ import (
 // Routes makes the connection between the different routes and their corresponding handler functions
 func Routes(r *http.ServeMux, db *sql.DB) {
 	uh := handlers.NewUserHandler(db)
-	r.HandleFunc("/users/new", uh.SignUpHandler)
-	r.HandleFunc("/users/login", uh.LogInHandler)
-	r.HandleFunc("/users/logout", uh.LogOutHandler)
+	r.HandleFunc("POST /users/new", uh.SignUpHandler)
+	r.HandleFunc("POST /users/login", uh.LogInHandler)
+	r.HandleFunc("DELETE /users/logout", uh.LogOutHandler)
 
 	ph := handlers.NewProductHandler(db)
-	r.HandleFunc("products/", ph.GetProduct)
-	r.HandleFunc("products", ph.GetListProducts)
-	r.HandleFunc("products/new", ph.PostNewProduct)
-	r.HandleFunc("products/", ph.DeleteOrSellProduct)
+	r.HandleFunc("GET products/", ph.GetProduct)
+	r.HandleFunc("GET products", ph.GetListProducts)
+	r.HandleFunc("POST products/new", ph.PostNewProduct)
+	r.HandleFunc("DELETE products/", ph.DeleteOrSellProduct)
 
 	bh := handlers.NewBidHandler(db)
-	r.HandleFunc("bids", bh.PostBid)
-	r.HandleFunc("bids/", bh.DeleteBid)
+	r.HandleFunc("POST bids", bh.PostBid)
+	r.HandleFunc("DELETE bids/", bh.DeleteBid)
 
 	foh := handlers.NewFollowerHandler(db)
-	r.HandleFunc("followers/follow", foh.GetUsersImFollowing)
-	r.HandleFunc("followers/follow/", foh.FollowUser)
-	r.HandleFunc("followers/follow/", foh.UnfollowUser)
+	r.HandleFunc("GET followers/follow", foh.GetUsersImFollowing)
+	r.HandleFunc("POST followers/follow/", foh.FollowUser)
+	r.HandleFunc("DELETE followers/follow/", foh.UnfollowUser)
 
 	fah := handlers.NewFavoriteHandler(db)
-	r.HandleFunc("/favorites", fah.GetFavorites)
-	r.HandleFunc("/favorites/", fah.SaveFavorite)
-	r.HandleFunc("/favorites/", fah.DeleteFavorite)
+	r.HandleFunc("GET /favorites", fah.GetFavorites)
+	r.HandleFunc("POST /favorites/", fah.SaveFavorite)
+	r.HandleFunc("DELETE /favorites/", fah.DeleteFavorite)
 
-	r.Handle("/", http.FileServer(http.Dir("./static/")))
+	// TODO maybe its better to sub my toy path param parser with the official recent addition to the SL
+	r.Handle("/{$}", http.FileServer(http.Dir("./static/")))
 }
